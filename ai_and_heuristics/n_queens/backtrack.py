2a05e1ca4d42607ffd20ffb252e97aad1ad516ed
Josh Cunningham <josh.cu@gmail.com>
2022-09-12 12:13:34 -0400

    Add rubric appeasing comments

--- ai_and_heuristics/n_queens/backtrack.py
+++ ai_and_heuristics/n_queens/backtrack.py
@@ -1,15 +1,26 @@
+"""
+This script is designed to solve constrained n-queens using a backtracking algorithm.
+All the functions not directly related to backtracking have been moved to the helpers module.
+This should make the code much easier to follow.
+"""
+
+__author__ = 'Josh Cunningham'
+__copyright__ = 'Copyright 2022, constrained n-queens'
+__email__ = 'Josh.Cu@gmail.com'
+
 import helpers
 import sys
 import time
 
 sys.setrecursionlimit(10**9)
-start_time = time.time()
+# please forgive this global,
+# python doesn't have real constants without using something like pydantic
+START_TIME = time.time()
 
 
 def backtrack_grid(grid, frozen_column=-1):
-    print(frozen_column)
     '''
-    Back tracks grid as far as needed up graph.
+    Back tracks the grid as far as needed up graph.
     As we are searching by looping through columns left to right and then through rows top to bottom,
     we know that the previous position in the tree can be found by moving the queen in the left column down.
     If the queen cannot be moved down, then it is removed, 
@@ -25,6 +36,9 @@ def backtrack_grid(grid, frozen_column=-1):
     |0|0|0|0|                 |0|0|0|0|
     |0|x|x|0|                 |0|0|0|0|
 
+    For this to work for constrained N-Queens, we cannot backtrack the column with the first queen in it
+    To do this I just save the column number the first queen was in as frozen_column,
+    and skip it when looping over the columns
     '''
     rows = helpers.get_grid_width(grid)
     columns = rows
@@ -35,21 +49,21 @@ def backtrack_grid(grid, frozen_column=-1):
         for row_number in range(0, rows):
             if grid[row_number][column_number] == 1:
                 if row_number < rows - 1:
+                    # if the queen can be moved down, do that and stop
                     grid[row_number][column_number] = 0
                     grid[row_number + 1][column_number] = 1
                     return
                 else:
+                    # if not, remove the queen and check the next column
                     grid[row_number][column_number] = 0
 
-    # TODO this is not a great way of exiting
     # if we reach this point the grid can't be backtracked
     print("Grid can't be backtracked, no solution found")
     sys.exit(1)
 
 
 def place_next_queen(grid, frozen_column=-1):
-    # helpers.print_grid(grid)
-    # print('\n')
+
     rows = helpers.get_grid_width(grid)
     columns = rows
     placed = False
@@ -58,22 +72,21 @@ def place_next_queen(grid, frozen_column=-1):
         for row_number in range(0, rows):
 
             if grid[row_number][column_number] == 0:
-
+                # if no queen, place a queen
                 grid[row_number][column_number] = 1
-                valid_position = helpers.isValid(grid)
+                valid_position = helpers.is_valid(grid)
+
                 if valid_position:
                     # if it fits in the grid then return true
                     return True
                 elif not valid_position and row_number == rows - 1:
+                    # if not, backtracking and try again
                     backtrack_grid(grid, frozen_column)
                     return place_next_queen(grid, frozen_column)
                 else:
                     # if not valid and not on final row, backtrack last guess
-                    # by setting value back to zero
+                    # by setting value back to zero and looping to the next position
                     grid[row_number][column_number] = 0
-
-    # helpers.print_grid(grid)
-    # print('\n')
 
     return placed
 
@@ -81,14 +94,16 @@ def place_next_queen(grid, frozen_column=-1):
 def search_solutions(grid, frozen_column=-1):
     num_queens = helpers.count_queens(grid)
     grid_width = helpers.get_grid_width(grid)
-    if (num_queens == grid_width) and (helpers.isValid(grid)):
+
+    # if n queens are placed and the grid is valid
+    if (num_queens == grid_width) and (helpers.is_valid(grid)):
         print("Solution Found!")
         helpers.print_grid(grid)
         print('\n')
         helpers.save_grid(grid)
-        print("--- %s seconds ---" % (time.time() - start_time))
+        print("--- %s seconds ---" % (time.time() - START_TIME))
         sys.exit(0)
-    elif (num_queens == grid_width) and not (helpers.isValid(grid)):
+    elif (num_queens == grid_width) and not (helpers.is_valid(grid)):
         print("no solution found")
         sys.exit(1)
 
@@ -102,9 +117,11 @@ def search_solutions(grid, frozen_column=-1):
 
 
 if __name__ == "__main__":
-
-    #grid = helpers.init_grid(10)
+    frozen_column = -1
     grid, frozen_column = helpers.load_grid("input.csv")
-    print(frozen_column)
+
+    # uncomment here if you want to try a grid with no input
+    #grid = helpers.init_grid(10)
+
     helpers.print_grid(grid)
     search_solutions(grid, frozen_column)
--- ai_and_heuristics/n_queens/helpers.py
+++ ai_and_heuristics/n_queens/helpers.py
@@ -1,8 +1,22 @@
-def get_grid_width(grid):
+"""
+This file contains a series of functions to assist with the nQueens problem.
+As the focus of this lab was the implemntation of backtracking, all other helper functions are located here.
+
+"""
+
+__author__ = 'Josh Cunningham'
+__copyright__ = 'Copyright 2022, constrained n-queens'
+__email__ = 'Josh.Cu@gmail.com'
+
+
+def get_grid_width(grid: dict):
     return len(grid.keys())
 
 
 def print_grid(grid: dict):
+    '''
+    Helper function to print the grid out to console
+    '''
     rows = get_grid_width(grid)
     columns = rows
     for column_number in range(0, rows):
@@ -13,6 +27,10 @@ def print_grid(grid: dict):
 
 
 def load_grid(filename):
+    '''
+    Helper function to load the input.csv and set the frozen column
+    Takes relative filepath and returns grid, frozen_column
+    '''
     with open(filename, 'r', encoding='utf-8') as f:
         line = f.readline().strip().split(',')
         width = len(line)
@@ -31,6 +49,9 @@ def load_grid(filename):
 
 
 def save_grid(grid: dict):
+    '''
+    Helper function to save grid dictionary to a file called solution.csv
+    '''
     rows = get_grid_width(grid)
     columns = rows
     with open("solution.csv", 'w', encoding='utf-8') as f:
@@ -44,6 +65,9 @@ def save_grid(grid: dict):
 
 
 def init_grid(width):
+    '''
+    Helper to initialise and empty nested dictionary grid structure
+    '''
     grid = {}
     for column_number in range(0, width):
         grid[column_number] = {}
@@ -52,7 +76,9 @@ def init_grid(width):
     return grid
 
 
-def count_queens(grid):
+def count_queens(grid: dict):
+    '''
+    Helper that returns the total number of queens in a grid'''
     rows = get_grid_width(grid)
     columns = rows
     queens = 0
@@ -62,111 +88,26 @@ def count_queens(grid):
     return queens
 
 
-def __evaluate_columns(grid) -> bool:
-    rows = get_grid_width(grid)
-    columns = rows
-    for column_number in range(0, columns):
-        queens = 0
-        for row_number in range(0, rows):
-            queens += grid[column_number][row_number]
-        if queens > 1:
-            return False
-    return True
-
-
-def __evaluate_rows(grid) -> bool:
-    rows = get_grid_width(grid)
-    columns = rows
-    for row_number in range(0, rows):
-        queens = 0
-        for column_number in range(0, columns):
-            queens += grid[column_number][row_number]
-        if queens > 1:
-            return False
-    return True
-
-
-def __evaluate_diagonals(grid) -> bool:
-    rows = get_grid_width(grid)
-    columns = rows
-    # calculate left to right diagonals
-    # |x|0|0|
-    # |0|x|0|
-    # |0|0|x|
-    for column_number in range(0, columns):
-        row = 0
-        column = column_number
-        queens = 0
-        while (row < rows and column < columns):
-            queens += grid[column][row]
-            row += 1
-            column += 1
-        if queens > 1:
-            return False
-
-    for row_number in range(0, rows):
-        row = row_number
-        column = 0
-        queens = 0
-        while (row < rows and column < columns):
-            queens += grid[column][row]
-            row += 1
-            column += 1
-        if queens > 1:
-            return False
-
-    # calculate right to left diagonals
-    # |0|0|x|
-    # |0|x|0|
-    # |x|0|0|
-
-    for column_number in range(0, columns):
-        row = 0
-        column = column_number
-        queens = 0
-        while (row < rows and column >= 0):
-            queens += grid[column][row]
-            row += 1
-            column -= 1
-        if queens > 1:
-            return False
-
-    for row_number in range(0, rows):
-        row = row_number
-        column = columns - 1
-        queens = 0
-        while (row < rows and column >= 0):
-            queens += grid[column][row]
-            row += 1
-            column -= 1
-        if queens > 1:
-            return False
-
-    # if all diagonals don't have more than one queen
-    return True
-
-
-def evaluate_positions(grid) -> bool:
-    return __evaluate_columns(grid) and __evaluate_rows(grid) and __evaluate_diagonals(grid)
-
-
 # My original checker was too expensive so I'm reusing Dr.Duans
 # I did ask her if this was ok beforehand
 # check if a queen at [row][col] is attacked. We need to check only left for queen's safety.
-def isSafe(board, row, col, N):
+def _is_safe(grid, row, col, N):
+    '''
+    private method used by is_valid to determing if queen at a given position is safe
+    takes grid, row number, column number, total grid width returns True if safe, else False'''
     # Check this row on left side
     for i in range(col):
-        if board[row][i] == 1:
+        if grid[row][i] == 1:
             return False
 
     # Check upper diagonal on left
     for i, j in zip(range(row-1, -1, -1), range(col-1, -1, -1)):
-        if board[i][j] == 1:
+        if grid[i][j] == 1:
             return False
 
     # Check lower diagonal on left
     for i, j in zip(range(row+1, N, 1), range(col-1, -1, -1)):
-        if board[i][j] == 1:
+        if grid[i][j] == 1:
             return False
 
     return True
@@ -174,20 +115,27 @@ def isSafe(board, row, col, N):
 # checking if the solution is valid
 
 
-def isValid(grid):
+def is_valid(grid):
+    '''
+    Helper function to check if all the queens placed on a grid are not able to attack eachother
+    Takes grid, returns True or False if the grid is valid or not
+    '''
     N = get_grid_width(grid)
     for row in range(N):
         Qs = 0
         for col in range(N):
             if grid[row][col] == 1:
                 Qs += 1
-                if not isSafe(grid, row, col, N):
+                if not _is_safe(grid, row, col, N):
                     return False
 
     return True
 
 
-def get_first_empty_column(grid):
+def get_first_empty_column(grid: dict):
+    '''
+    Helper function that returns the first column without a queen from left to right
+    Takes grid, returns column index number e.g. first column == 0'''
     rows = get_grid_width(grid)
     columns = rows
     for column_number in range(0, columns):

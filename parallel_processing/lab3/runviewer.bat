ea4580b2bc4999ef58a34016a886cfa252960855
JoshCu <Josh.Cu@gmail.com>
2022-09-19 10:46:41 -0400

    add files for parallel processing lab 2&3

---
+++ parallel_processing/lab3/runviewer.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.7\bin\nvcc" -ccbin "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\bin\Hostx86\x64" -I "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.7\include" viewer.cu -l freeglut -o viewer.exe
+
+viewer.exe

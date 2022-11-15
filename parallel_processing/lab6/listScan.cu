d3556f021684a819fb5a5e3a54eb21959317a5af
Josh Cunningham <josh.cu@gmail.com>
2022-11-14 22:08:43 -0500

    add lab6 kernels

--- parallel_processing/lab6/listScan.cu
+++ parallel_processing/lab6/listScan.cu
@@ -32,16 +32,64 @@
 // Output its prefix sum = {lst[0], lst[0] + lst[1], lst[0] + lst[1] + ...
 // + lst[n-1]}
 
-#define BLOCK_SIZE 512 //@@ You can change this
+#define BLOCK_SIZE 256 //@@ You can change this
 
-__global__ void scan(int *input, int *output, int *aux, int len) {
+__device__ void reduction(int *input, int n)
+{
+  // XY[2*BLOCK_SIZE] is in shared memory
+  for (unsigned int stride = 1; stride <= BLOCK_SIZE; stride *= 2)
+  {
+    int index = (threadIdx.x + 1) * stride * 2 - 1;
+    if (index < 2 * BLOCK_SIZE)
+      input[index] += input[index - stride];
+    __syncthreads();
+  }
+}
+
+__device__ void post_reduction(int *input, int n)
+{
+  for (unsigned int stride = BLOCK_SIZE / 2; stride > 0; stride /= 2)
+  {
+    __syncthreads();
+    int index = (threadIdx.x + 1) * stride * 2 - 1;
+    if (index + stride < 2 * BLOCK_SIZE)
+    {
+      input[index + stride] += input[index];
+    }
+  }
+}
+
+__global__ void scan(int *input, int *output, int *aux, int len)
+{
   //@@ Modify the body of this function to complete the functionality of
   //@@ the scan on the device
   //@@ You may need multiple kernel calls; write your kernels before this
   //@@ function and call them from here
+  // load input into shared memory
+  __shared__ int XY[2 * BLOCK_SIZE];
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  if (index < len)
+  {
+    XY[threadIdx.x] = input[index];
+    XY[threadIdx.x + BLOCK_SIZE] = 0;
+  }
+  else
+  {
+    XY[threadIdx.x] = 0;
+    XY[threadIdx.x + BLOCK_SIZE] = 0;
+  }
+  __syncthreads();
+
+  reduction(XY, len);
+  post_reduction(XY, len);
+  __syncthreads();
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < len)
+    output[i] = XY[threadIdx.x];
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   int *hostInput;  // The input 1D list
   int *hostOutput; // The output list
   int *expectedOutput;
@@ -49,7 +97,7 @@ int main(int argc, char **argv) {
   int *deviceOutput;
   int *deviceAuxArray, *deviceAuxScannedArray;
   int numElements; // number of elements in the list
-  
+
   FILE *infile, *outfile;
   int inputLength, outputLength;
   StopWatchLinux stw;
@@ -58,73 +106,80 @@ int main(int argc, char **argv) {
   // Import host input data
   stw.start();
   if ((infile = fopen("input.raw", "r")) == NULL)
-  { printf("Cannot open input.raw.\n"); exit(EXIT_FAILURE); }
+  {
+    printf("Cannot open input.raw.\n");
+    exit(EXIT_FAILURE);
+  }
   fscanf(infile, "%i", &inputLength);
   hostInput = (int *)malloc(sizeof(int) * inputLength);
   for (int i = 0; i < inputLength; i++)
-     fscanf(infile, "%i", &hostInput[i]);
+    fscanf(infile, "%i", &hostInput[i]);
   fclose(infile);
   numElements = inputLength;
   hostOutput = (int *)malloc(numElements * sizeof(int));
   stw.stop();
   printf("Importing data and creating memory on host: %f ms\n", stw.getTime());
 
-  if (blog) printf("*** The number of input elements in the input is %i\n", numElements);
+  if (blog)
+    printf("*** The number of input elements in the input is %i\n", numElements);
 
   stw.reset();
   stw.start();
-  
+
   cudaMalloc((void **)&deviceInput, numElements * sizeof(int));
   cudaMalloc((void **)&deviceOutput, numElements * sizeof(int));
 
   cudaMalloc(&deviceAuxArray, (BLOCK_SIZE << 1) * sizeof(int));
   cudaMalloc(&deviceAuxScannedArray, (BLOCK_SIZE << 1) * sizeof(int));
-  
+
   stw.stop();
   printf("Allocating GPU memory: %f ms\n", stw.getTime());
 
   stw.reset();
   stw.start();
-  
+
   cudaMemset(deviceOutput, 0, numElements * sizeof(int));
-  
+
   stw.stop();
   printf("Clearing output memory: %f ms\n", stw.getTime());
 
   stw.reset();
   stw.start();
-  
+
   cudaMemcpy(deviceInput, hostInput, numElements * sizeof(int),
-                     cudaMemcpyHostToDevice);
+             cudaMemcpyHostToDevice);
 
   stw.stop();
   printf("Copying input memory to the GPU: %f ms\n", stw.getTime());
 
   //@@ Initialize the grid and block dimensions here
+  dim3 dimGrid(1, 1, 1);
+  dim3 dimBlock(BLOCK_SIZE, 1, 1);
 
   stw.reset();
   stw.start();
-  
+
   //@@ Modify this to complete the functionality of the scan
   //@@ on the device
+  scan<<<dimGrid, dimBlock>>>(deviceInput, deviceOutput, deviceAuxArray, numElements);
 
   cudaDeviceSynchronize();
- 
+
   stw.stop();
   printf("Performing CUDA computation: %f ms\n", stw.getTime());
 
   stw.reset();
   stw.start();
-  
+
   cudaMemcpy(hostOutput, deviceOutput, numElements * sizeof(int),
-                     cudaMemcpyDeviceToHost);
-  
+             cudaMemcpyDeviceToHost);
+
   stw.stop();
   printf("Copying output memory to the CPU: %f ms\n", stw.getTime());
 
   stw.reset();
   stw.start();
-  
+
   cudaFree(deviceInput);
   cudaFree(deviceOutput);
   cudaFree(deviceAuxArray);
@@ -134,22 +189,28 @@ int main(int argc, char **argv) {
   printf("Freeing GPU Memory: %f ms\n", stw.getTime());
 
   if ((outfile = fopen("output.raw", "r")) == NULL)
-  { printf("Cannot open output.raw.\n"); exit(EXIT_FAILURE); }
-  fscanf(outfile, "%i", &outputLength);
-  expectedOutput = (int *)malloc(sizeof(int) * outputLength);  
-  for (int i = 0; i < outputLength; i++)
-     fscanf(outfile, "%i", &expectedOutput[i]);	
-  fclose(outfile);
-  
-  int test = 1;
-  for (int i = 0; i < outputLength; i++) {
-     if (expectedOutput[i] != hostOutput[i])
-        printf("%i %i %i\n", i, expectedOutput[i], hostOutput[i]);
-     test = test && (expectedOutput[i] == hostOutput[i]);
+  {
+    printf("Cannot open output.raw.\n");
+    exit(EXIT_FAILURE);
   }
-  
-  if (test) printf("Results correct.\n");
-  else printf("Results incorrect.\n");
+  fscanf(outfile, "%i", &outputLength);
+  expectedOutput = (int *)malloc(sizeof(int) * outputLength);
+  for (int i = 0; i < outputLength; i++)
+    fscanf(outfile, "%i", &expectedOutput[i]);
+  fclose(outfile);
+
+  int test = 1;
+  for (int i = 0; i < outputLength; i++)
+  {
+    if (expectedOutput[i] != hostOutput[i])
+      printf("%i %i %i\n", i, expectedOutput[i], hostOutput[i]);
+    test = test && (expectedOutput[i] == hostOutput[i]);
+  }
+
+  if (test)
+    printf("Results correct.\n");
+  else
+    printf("Results incorrect.\n");
 
   free(hostInput);
   cudaFreeHost(hostOutput);
